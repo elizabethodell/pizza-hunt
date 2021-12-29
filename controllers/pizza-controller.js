b21a6099fab17d43f28dd8faa37d8496ef9e61ae
ebo16@vt.edu <ebo16@vt.edu>
2021-12-28 19:51:58 -0500

    validators added

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -18,26 +18,26 @@ const pizzaController = {
     },
 
     // get one pizza by id
-   // get one pizza by id
-getPizzaById({ params }, res) {
-    Pizza.findOne({ _id: params.id })
-      .populate({
-        path: 'comments',
-        select: '-__v'
-      })
-      .select('-__v')
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
+    // get one pizza by id
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // createPizza POST /api/pizzas
     createPizza({ body }, res) {
@@ -47,8 +47,9 @@ getPizzaById({ params }, res) {
     },
 
     // update pizza by id PUT /api/pizzas/:id
+    // update pizza by id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
